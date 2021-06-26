4569e67bf42a817f21c569cab63b976e2ff062fa
Law Zava <law@zava.dev>
2021-06-26 19:19:45 +0300

    :sparkles: add support for output type specification

--- go.mod
+++ go.mod
@@ -3,21 +3,17 @@ module github.com/lawzava/scrape
 go 1.16
 
 require (
-	github.com/PuerkitoBio/goquery v1.6.1 // indirect
-	github.com/andybalholm/cascadia v1.2.0 // indirect
-	github.com/antchfx/htmlquery v1.2.3 // indirect
-	github.com/antchfx/xmlquery v1.3.5 // indirect
-	github.com/antchfx/xpath v1.1.11 // indirect
-	github.com/chromedp/chromedp v0.6.8
-	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gocolly/colly v1.2.0
-	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/PuerkitoBio/goquery v1.7.0 // indirect
+	github.com/antchfx/xmlquery v1.3.6 // indirect
+	github.com/antchfx/xpath v1.2.0 // indirect
+	github.com/chromedp/cdproto v0.0.0-20210625233425-810000e4a4fc // indirect
+	github.com/chromedp/chromedp v0.7.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/lawzava/emailscraper v1.1.0
-	github.com/lawzava/go-tld v1.0.0
-	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/spf13/cobra v1.1.3
-	github.com/temoto/robotstxt v1.1.1 // indirect
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
-	golang.org/x/sys v0.0.0-20210305230114-8fe3ee5dd75b // indirect
-	golang.org/x/text v0.3.5 // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+	google.golang.org/protobuf v1.27.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/lawzava/emailscraper"
-
 	"github.com/spf13/cobra"
 )
 
@@ -21,6 +21,14 @@ func main() {
 var (
 	scraperParameters emailscraper.Config
 	url               string
+	output            string
+	outputWithURL     bool
+)
+
+const (
+	outputPlain = "plain"
+	outputCSV   = "csv"
+	outputJSON  = "json"
 )
 
 // nolint:exhaustivestruct,gochecknoglobals // not valid requirement for this use case
@@ -37,8 +45,9 @@ var rootCmd = &cobra.Command{
 			log.Fatal(err)
 		}
 
-		for _, email := range scrapedEmails {
-			fmt.Println(email) // nolint:forbidigo // allow println here for non intrusive response
+		err = handleOutput(url, scrapedEmails)
+		if err != nil {
+			log.Fatal(err)
 		}
 	},
 }
@@ -61,4 +70,68 @@ func init() {
 		"js", false, "Enables EnableJavascript execution await")
 	rootCmd.PersistentFlags().IntVar(&scraperParameters.Timeout,
 		"timeout", 0, "If > 0, specify a timeout (seconds) for js execution await")
+	rootCmd.PersistentFlags().StringVar(&output,
+		"output", outputPlain, "Output type to use (default 'plain', supported: 'csv', 'json')")
+	rootCmd.PersistentFlags().BoolVar(&outputWithURL,
+		"output-with-url", false, "Adds URL to output with each email")
+}
+
+// nolint:forbidigo // allow println here for non intrusive response
+func handleOutput(url string, emails []string) error {
+	switch output {
+	case outputCSV:
+		for _, email := range emails {
+			if outputWithURL {
+				fmt.Print(url + ",")
+			}
+
+			fmt.Println(email)
+		}
+	case outputJSON:
+		if outputWithURL {
+			type outputFormat struct {
+				URL   string `json:"url"`
+				Email string `json:"email"`
+			}
+
+			out := make([]outputFormat, len(emails))
+			for i, email := range emails {
+				out[i].URL = url
+				out[i].Email = email
+			}
+
+			b, err := json.Marshal(out)
+			if err != nil {
+				return fmt.Errorf("failed to marshal json with url response: %w", err)
+			}
+
+			fmt.Println(string(b))
+		} else {
+			type outputFormat struct {
+				Email string `json:"email"`
+			}
+
+			out := make([]outputFormat, len(emails))
+			for i, email := range emails {
+				out[i].Email = email
+			}
+
+			b, err := json.Marshal(out)
+			if err != nil {
+				return fmt.Errorf("failed to marshal json response: %w", err)
+			}
+
+			fmt.Println(string(b))
+		}
+	default:
+		for _, email := range emails {
+			if outputWithURL {
+				fmt.Print(url + " ")
+			}
+
+			fmt.Println(email)
+		}
+	}
+
+	return nil
 }
